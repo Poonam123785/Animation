ac66f0ed5b3971075f99f4946778461ab37039cb
Poonam123785 <MPS.saini@outlook.com>
2025-02-01 16:32:42 +0530

    Add project files.

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#mytext', {
+    strings: ['Javascript Animations', 'Javascript Animations', 'Javascript Animations 2nd time'], 
+    typeSpeed: 100
+});
+
+AOS.init({
+    duration:2000
+})
